3077a5886ea3e80d2f4f0659cfa508874ee0f3f5
lxl-s <1195849758@qq.com>
2023-07-24 16:44:54 +0800

    update:尽责性

--- src/pages/Layout/Result/ResultIntelligence/psychological/trace/index.js
+++ src/pages/Layout/Result/ResultIntelligence/psychological/trace/index.js
@@ -2,6 +2,44 @@ import React from "react";
 
 
 const PsychologicalTracePage = () => {
+    const data={
+        nodes:[
+            {
+                id:"社会属性空间->互联网社交空间",
+                label:"社会属性空间->互联网社交空间",
+                socialAttributes:[
+                    {
+                        key: "真实姓名",
+                        value: "Aaron Esau",
+                        url: "https://aaronesau.com/"
+
+                    },
+                ]
+            },
+            {
+                id:"社会属性空间->互联网工作空间",
+                label:"社会属性空间->互联网社交空间",
+                socialAttributes:[
+                
+                ]
+            },
+            {
+                id:"性格特质空间->互联网学习空间",
+                label:"性格特质空间->互联网学习空间",
+                socialAttributes:[
+                
+                ]
+            },
+            {
+                id:"性格特质空间->互联网工作空间",
+                label:"",
+            },
+            {
+                id:"互联网娱乐空间->互联网社交空间",
+                label:"",
+            }            
+        ]
+    }
     return (
         <></>
     )
